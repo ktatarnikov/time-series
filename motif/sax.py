4a99e99ff054fc6e40165f16e25cb2a3bcd5dd8e
ktatarnikov <ktatarnikov@stackstate.com>
2019-06-10 15:09:58 +0200

    documenting sax

--- motif/sax.py
+++ motif/sax.py
@@ -3,9 +3,21 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 class SAX:
-    def __init__(self, alpha, symbol_size, paa_size = None, window_size = None):
+    '''
+    SAX: Symbolic Aggregate Representation of time series.
+    https://jmotif.github.io/sax-vsm_site/morea/algorithm/SAX.html
+
+    Parameters
+    ----------
+    alpha : int
+        the number of breakpoints
+    paa_size: int
+        the size of PAA window in points
+    window_size: int
+        the size of sliding window in points
+    '''
+    def __init__(self, alpha, paa_size = None, window_size = None):
         self.alpha = alpha
-        self.symbol_size = symbol_size
         self.window_size = window_size
         self.paa_size = paa_size
         # 1/(α −1),2/(α −1),...,(α −2)/(α −1)
@@ -14,13 +26,24 @@ class SAX:
         self.symbols = "abcdefghijklmnopqrstuvwxyz"
 
     def encode(self, series: np.array):
-        # series = StandardScaler().fit_transform(series.reshape(-1, 1)).squeeze()
+        """SAX-PAA encoding of input timeseries.
+        Args:
+            input: numpy array with timeseries
+        Returns:
+            numpy array of encoded windows
+        """
         windows = self._to_windows(series)
         std = self._to_std(windows)
         paa = self._to_paa_windows(std)
         return self._quantize_windows(paa)
 
     def _to_windows(self, series):
+        """Splitting timeseries into a number windows using sliding window method with step 1 point.
+        Args:
+            series: numpy array of series
+        Returns:
+            np array windows
+        """
         result = []
         for i in range(0, len(series) - self.window_size + 1, 1):
             window = series[i : (i + self.window_size)]
@@ -28,6 +51,12 @@ class SAX:
         return np.array(result)
 
     def _to_std(self, windows):
+        """Scaling windows using z-normalization. Each window is scaled separately.
+        Args:
+            windows: list of windows each window is np.array
+        Returns:
+            np array windows
+        """
         results = []
         for window in windows:
             result = StandardScaler().fit_transform(window.reshape(-1, 1))
@@ -36,12 +65,25 @@ class SAX:
         return np.array(results)
 
     def _to_paa_windows(self, windows):
+        """Applying PAA to each window.
+        Args:
+            input: the list of windows - numpy arrays
+        Returns:
+            np array of paa-transformed windows
+        """
         results = []
         for window in windows:
             results.append(self._to_paa(window))
         return np.array(results)
 
     def _to_paa(self, series):
+        """Dimensionality reduction using PAA (Piecewise Aggregate Approximation)
+            E.g. https://jmotif.github.io/sax-vsm_site/morea/algorithm/PAA.html
+        Args:
+            input: numpy array - window
+        Returns:
+            np array holding paa transformed window
+        """
         series_len = len(series)
         if (series_len == self.paa_size):
             return np.copy(series)
@@ -61,12 +103,25 @@ class SAX:
                 return res / series_len
 
     def _quantize_windows(self, windows):
+        """Transform input list of windows into symbolic representation.
+        Args:
+            windows: the list of windows
+        Returns:
+            np array if SAX words
+        """
         results = []
         for window in windows:
             results.append(self._quantize(window))
         return np.array(results)
 
     def _quantize(self, series: np.array):
+        """Quantize input series into SAX representation.
+        Args:
+            series: numpy array
+                input window of points
+        Returns:
+            string word in SAX form
+        """
         word = ""
         for point in series:
             symbol = self._point_to_symbol(point)
@@ -74,6 +129,12 @@ class SAX:
         return word
 
     def _point_to_symbol(self, point):
+        """Converts point into symbolic representation.
+        Args:
+            point: single metric value
+        Returns:
+            the symbol that corresponds to this value level
+        """
         for idx, value in enumerate(self.breakpoints):
             if point < value:
                 return self.symbols[idx]
--- motif/test_sax.py
+++ motif/test_sax.py
@@ -7,30 +7,30 @@ from .sax import SAX
 class TestSAX(unittest.TestCase):
 
   def test_to_windows(self):
-      sax = SAX(alpha = 3, symbol_size=3, window_size = 3)
-      actual = sax._to_windows(np.array([1,1,1,2,2]))
-      expected = np.array([[1,1,1],[1,1,2],[1,2,2]])
+      sax = SAX(alpha = 3, window_size = 3)
+      actual = sax._to_windows(np.array([1,1,1,2,2], dtype=np.float64))
+      expected = np.array([[1,1,1],[1,1,2],[1,2,2]], dtype=np.float64)
       testing.assert_array_equal(expected, actual)
 
   def test_to_std(self):
-      sax = SAX(alpha = 3, symbol_size=3, window_size = 3)
-      actual = sax._to_std(np.array([[1,2,3],[2,3,4],[3,4,5]]))
-      expected = np.array([[-1.224745,0,1.224745],[-1.224745,0,1.224745],[-1.224745,0,1.224745]])
+      sax = SAX(alpha = 3, window_size = 3)
+      actual = sax._to_std(np.array([[1,2,3],[2,3,4],[3,4,5]], dtype=np.float64))
+      expected = np.array([[-1.224745,0,1.224745],[-1.224745,0,1.224745],[-1.224745,0,1.224745]], dtype=np.float64)
       testing.assert_array_almost_equal(expected, actual)
 
   def test_to_paa(self):
-      sax = SAX(alpha = 3, symbol_size=3, paa_size = 3)
+      sax = SAX(alpha = 3, paa_size = 3)
       actual = sax._to_paa_windows(np.array([[-2, 0, 2, 0, -1],[2, 3, 4, 4, 3, 2]]))
-      expected = np.array([[-1.2, 1.2, -0.6],[2.5, 4, 2.5]])
+      expected = np.array([[-1.2, 1.2, -0.6],[2.5, 4, 2.5]], dtype=np.float64)
       testing.assert_array_almost_equal(expected, actual)
 
   def test_encode_success(self):
-      sax = SAX(alpha = 3, symbol_size=3, window_size = 5, paa_size = 3)
+      sax = SAX(alpha = 3, window_size = 5, paa_size = 3)
       expected = np.array(["abc", "abc", "abc", "abc", "abc"])
-      actual = sax.encode(np.array([1,1,1,2,2,2,3,3,3]))
+      actual = sax.encode(np.array([1,1,1,2,2,2,3,3,3], dtype=np.float64))
       testing.assert_array_equal(expected, actual)
 
-      sax = SAX(alpha = 4, symbol_size=3, window_size = 5, paa_size = 3)
+      sax = SAX(alpha = 4, window_size = 5, paa_size = 3)
       expected = np.array([
         "abd",
         "acd",
@@ -41,5 +41,5 @@ class TestSAX(unittest.TestCase):
         "abd",
         "acd"
       ])
-      actual = sax.encode(np.array([1,1,1,2,2,2,3,3,3,4,4,4]))
+      actual = sax.encode(np.array([1,1,1,2,2,2,3,3,3,4,4,4], dtype=np.float64))
       testing.assert_array_equal(expected, actual)
